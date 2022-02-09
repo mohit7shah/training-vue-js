28c2f2ff338670b16766a14b117e314a510af606
mohit7shah <mohit.shah@bacancy.com>
2022-02-10 01:35:46 +0530

    Session-2

--- Session-2/basics-05-using-the-native-event-object/app.js
+++ Session-2/basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      lastname: "",
+      //fullname: "",
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 100);
+      }
+    },
+    // 100 shows the time break to set 0 in counter
+
+    /*name(value) {
+      if (this.name === "") {
+        this.fullname = " ";
+      } else {
+        this.fullname = value + " " + this.lastName;
+      }
+    },
+    lastName(value) {
+      if (this.name === "") {
+        this.fullname = " ";
+      } else {
+        this.fullname = this.name + " " + value;
+      }
+    },*/
+  },
+  // dependency is required in the computed part.
+  computed: {
+    fullname() {
+      console.log("this only print if you write input in the feature.");
+      if (this.name === "" || this.lastname === "") {
+        return " ";
+      }
+      return this.name + " " + this.lastname;
+    },
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    //outputfullname() {},
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +54,11 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
