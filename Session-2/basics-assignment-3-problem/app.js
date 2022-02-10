07982c0f5c75de1492a4d6b572deb24389c1d9b5
mohit7shah <mohit.shah@bacancy.com>
2022-02-10 18:26:03 +0530

    Session - 2

---
+++ Session-2/basics-assignment-3-problem/app.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      //RESULT: 0,
+      result: 0,
+    };
+  },
+  watch: {
+    RESULT() {
+      if (this.result === 37) {
+        const that = this;
+        setTimeout(function () {
+          that.result = 0;
+        }, 5);
+      }
+    },
+  },
+  computed: {
+    RESULT() {
+      console.log("running  " + this.result);
+      if (this.result > 37) {
+        return "Too much";
+      } else if (this.result < 37) {
+        return "Not there yet";
+      }
+    },
+  },
+  methods: {
+    add(num) {
+      this.result = this.result + num;
+    },
+  },
+});
+
+app.mount("#assignment");
