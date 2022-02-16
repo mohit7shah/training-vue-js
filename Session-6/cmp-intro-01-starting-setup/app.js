b3701001cf2804af78a644bfc8beabddbead2bfd
mohit7shah <mohit.shah@bacancy.com>
2022-02-16 23:43:45 +0530

    Session - 6

--- Session-6/cmp-intro-01-starting-setup/app.js
+++ Session-6/cmp-intro-01-starting-setup/app.js
@@ -50,4 +50,34 @@ app.component("friend-contact", {
   },
 });
 
+app.component("user-contact", {
+  template: `
+  <li>
+  <h2>{{ friend.name }}</h2>
+  <button @click="toggleDetails()">
+    {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+  </button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong> {{ friend.phone }}</li>
+    <li><strong>Email:</strong> {{ friend.email }}</li>
+  </ul>
+</li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "julie",
+        name: "Julie Jones",
+        phone: "09876 543 221",
+        email: "julie@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
 app.mount("#app");
