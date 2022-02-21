d99d5ca001938fd46c45a2a25dd56fa69377dcb9
mohit7shah <mohit.shah@bacancy.com>
2022-02-21 17:06:12 +0530

    Session - 7

---
+++ Session-7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      FriendContact: [
+        {
+          id: "mohit",
+          name: "Mohit",
+          phone: "0123456789",
+          email: "hello@gmail.com",
+        },
+        {
+          id: "raj",
+          name: "Raj",
+          phone: "01123456789",
+          email: "rajs@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Session-7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone : </strong> {{ friend.phone }}</li>
+      <li><strong>Email : </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "mohit",
+        name: "Mohit",
+        phone: "0123456789",
+        email: "hello@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
