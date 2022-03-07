39cf777ca49751d0a40dfeb7e31e1ea70f8d8923
mohit7shah <mohit.shah@bacancy.com>
2022-03-07 10:42:32 +0530

    Session-15

--- Session-15/vuex-01-starting-setup/src/main.js
+++ Session-15/vuex-01-starting-setup/src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
-
+import { createStore } from 'vuex';
 import App from './App.vue';
 
-const app = createApp(App);
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increament(state) {
+      state.counter = state.counter + 2;
+    },
+  },
+});
 
+const app = createApp(App);
+app.use(store);
 app.mount('#app');
